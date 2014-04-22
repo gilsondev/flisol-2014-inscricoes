2be00ea1fd38fea2dd5eae62024d82c5b3845339
Douglas Miranda <douglasmirandasilva@gmail.com>
2014-04-22 01:35:03 -0400

    Removendo INTERCEPT_REDIRECTS. DeprecationWarning.

--- flisol_inscricoes/flisol_inscricoes/config/settings_local.py
+++ flisol_inscricoes/flisol_inscricoes/config/settings_local.py
@@ -6,10 +6,6 @@ INSTALLED_APPS += (
 
 INTERNAL_IPS = ('127.0.0.1',)
 
-DEBUG_TOOLBAR_CONFIG = {
-    'INTERCEPT_REDIRECTS': False,
-}
-
 MIDDLEWARE_CLASSES += (
     'debug_toolbar.middleware.DebugToolbarMiddleware',
 )
